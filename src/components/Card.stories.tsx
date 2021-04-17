169ec9c62413ed22eec2e3ec522e8f2a389d6970
Ryo Togashi <35892492+ryonryon@users.noreply.github.com>
2021-04-17 17:11:22 +0900

    Add tailwind (#1)

--- src/components/Card.stories.tsx
+++ src/components/Card.stories.tsx
@@ -5,4 +5,17 @@ export default {
   title: "Card",
 };
 
-export const example = () => <Card />;
+export const example = () => {
+  return (
+    <Card
+      avatar={{
+        src:
+          "https://lh3.googleusercontent.com/ogw/ADGmqu_ygiQbIuaW37QcUrCERmUpmhLkuDyBoraUdeVH3Q=s32-c-mo",
+        alt: "avatar image",
+      }}
+      title="Ryo Togashi"
+      content="buy one get one"
+      time="18:01"
+    />
+  );
+};
