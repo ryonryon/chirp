cf243275d67684b82ceba234f69a5ca9cf99dcd2
Ryo Togashi <35892492+ryonryon@users.noreply.github.com>
2021-04-18 21:30:22 +0900

    Add ChatTag

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -20,7 +20,7 @@ export default function Chat({ userId, message, sendAt, avatarImg }: Props) {
       />
 
       <div className="flex items-end mt-2">
-        <p className="inline-block bg-indigo-400 py-1 px-4 max-w-3xl rounded-lg mr-2 text-s font-light">
+        <p className="inline-block bg-indigo-400 py-1 px-4 max-w-3xl rounded-lg mr-2 text-sm font-light">
           {message}
         </p>
 
